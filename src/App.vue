e2006c53e244adb0f065fc09ac66e78c32273a05
Miranda <31425585+ephiet@users.noreply.github.com>
2021-03-22 10:21:09 -0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    
+    <calculadora msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import calculadora from './components/calculadora.vue'
+
+export default {
+  name: 'App',
+  components: {
+    calculadora
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  height:100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+body{
+  margin:0;
+  padding:0;
+  height:100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  
+}
+</style>
---
+++ src/components/calculadora.vue
@@ -0,0 +1,149 @@
+<template>
+    <div class="calcul">
+        <h4>Ingrese dos números y seleccione una operación</h4>
+
+        <form class="form">
+            <div>
+                <input v-model="opA" type="number" class="input-a">
+            </div>
+
+            <select v-model="operacion" class="select-operacion">
+                <option value="+">+</option>
+                <option value="-">-</option>
+                <option value="*">*</option>
+                <option value="/">/</option>
+            </select>
+
+            <div>
+                <input v-model="opB" type="number" class="input-b">
+            </div>
+            
+            <div>
+              <button @click.prevent="resultadoFinal()" type="submit" class="btn">Calcular</button>
+          </div>
+        </form>
+        <hr>
+        <div class="display-resultado">
+            <h4>Resultado:</h4>
+            <p class="display-operandoa">{{opA}}</p>
+            <p class="display-operacion">{{operacion}}</p>
+            <p class="display-operandob">{{opB}}</p>
+            <p class="display-igual">{{igual}}</p>
+            <p class="display-total">{{resultado}}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'calculadora',
+  props: {
+    msg: String
+  },
+  data: function() {
+    return {
+        opA: "",
+        opB: "",
+        operacion: "",
+        resultado: "",
+        igual: "",
+        
+    }},
+    methods: {
+        resultadoFinal() {
+          
+            if (this.resultado == 0){
+            switch (this.operacion) {
+              case "+":
+                this.resultado = parseInt(this.opA) + parseInt(this.opB);
+                break;
+              case "-":
+                this.resultado = parseInt(this.opA) - parseInt(this.opB);
+                break;
+              case "*":
+                this.resultado = parseInt(this.opA) * parseInt(this.opB);
+                break;
+              case "/":
+                this.resultado = parseInt(this.opA) / parseInt(this.opB);
+                break;
+            }
+            }
+            else {
+              this.resultado = 0;
+              switch (this.operacion) {
+              case "+":
+                this.resultado = parseInt(this.opA) + parseInt(this.opB);
+                break;
+              case "-":
+                this.resultado = parseInt(this.opA) - parseInt(this.opB);
+                break;
+              case "*":
+                this.resultado = parseInt(this.opA) * parseInt(this.opB);
+                break;
+              case "/":
+                this.resultado = parseInt(this.opA) / parseInt(this.opB);
+                break;
+            }
+            }
+            this.igual = "="
+        },
+        limpiar() {
+        this.opA= "";
+        this.opB= "";
+        this.operacion= "";
+        this.resultado= "";
+        this.igual= "";
+        },
+    }
+}
+</script>
+
+<style scoped>
+.calcul {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color:white;
+  padding: 4em;
+  margin-top: 4em;
+}
+.form {
+  display: flex;
+  flex-wrap: wrap;
+  margin-top: 2em;
+}
+.btn{
+  background-color: rgb(250, 101, 163);
+  color: white;
+  font-weight: bold;
+  margin: 0 10px 0 10px;
+  height: 60%;
+}
+.input-a, .input-b {
+  margin: 0 1em 2em 1em;
+  padding: 1em;
+}
+.select-operacion {
+  padding:0.5em;
+  margin: 0 1em 2em 1em;
+}
+.display-resultado{
+  display: flex;
+  flex-wrap: wrap;
+  width:100%;
+}
+.display-operandoa, .display-operandob, .display-total{
+  text-align: center;
+  margin: 1em 0.5em 1em 0.5em;
+  font-size: 2em;
+  font-family: monospace;
+ }
+.display-operacion, .display-igual{
+  text-align: center;
+  margin: 1em 0.5em 1em 0.5em;
+  font-family: monospace;
+  font-size: 2em;
+}
+
+</style>
